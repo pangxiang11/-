19d74ce4065ad2d9bd87d0448d6c851a0babbbff
qianfeng <qianfeng@qianfengdeMac-mini-182.local>
2014-10-16 14:40:03 +0800

    import ViewController

--- 数据库练习第5天/数据库练习第5天/AppDelegate.m
+++ 数据库练习第5天/数据库练习第5天/AppDelegate.m
@@ -7,13 +7,13 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    self.window.rootViewController = [[UINavigationController alloc]initWithRootViewController:[[ViewController alloc]init]];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
